298e2137c02f240125fe8227c6879e0930b3440c
Harviator <78506767+harviator@users.noreply.github.com>
2021-05-18 07:02:16 -0400

    added two arrays with numbers and lowercase letter

--- script.js
+++ script.js
@@ -12,3 +12,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//arrays
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
