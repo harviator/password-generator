79c103466ded078bf66ef30699ba9cbb2c1e0be4
Harviator <78506767+harviator@users.noreply.github.com>
2021-05-18 19:22:05 -0400

    I learned about methods today and concat is helpful to me right now.  Also, I realized I was missing a ton of .  Lastly, based on my internet searches there is way cleaner code than this, but I'm learning so whatever

--- script.js
+++ script.js
@@ -14,18 +14,21 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //arrays
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "~", "?", "<", ">", ":", ";", "/", " ", ".", ".", "{", "}"]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "~", "?", "<", ">", ":", ";", "/", " ", ".", ".", "{", "}"];
 
-console.log(numbers, lowercase, uppercase, symbols)
+console.log(numbers, lowercase, uppercase, symbols);
 
 //random number between 1-26
-console.log(Math.floor(Math.random() * 26))
+console.log(Math.floor(Math.random() * 26));
 
 //now I want to take one of my arrays and randomize it
-console.log(lowercase[Math.floor(Math.random() * 26)])
+console.log(lowercase[Math.floor(Math.random() * 26)]);
 
 //all my arrays randomized
-console.log(lowercase[Math.floor(Math.random() * 26)], numbers[Math.floor(Math.random() * 10)], uppercase[Math.floor(Math.random() * 26)], symbols[Math.floor(Math.random() * 26)])
+console.log(lowercase[Math.floor(Math.random() * 26)], numbers[Math.floor(Math.random() * 10)], uppercase[Math.floor(Math.random() * 26)], symbols[Math.floor(Math.random() * 26)]);
+
+//concat all my arrays
+var allCharacters = numbers.concat(lowercase, uppercase, symbols);
