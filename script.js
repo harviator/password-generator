63f6d12154f96e231e0c578f5710404b605eb7c4
Harviator <78506767+harviator@users.noreply.github.com>
2021-05-19 20:42:59 -0400

    Had a tutor session tonight, which really help get me through the difficulties I was having

--- script.js
+++ script.js
@@ -13,6 +13,15 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+var myNumber = "10";
+console.log(parseInt(myNumber));
+
+if (isNaN(parseInt(myNumber))) {
+  console.log("This is not a number");
+} else {
+  console.log("This is a number");
+}
+
 //arrays
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -46,14 +55,66 @@ var password = {
   lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
   upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
   symbols: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "~", "?", "<", ">", ":", ";", "/", " ", ".", ".", "{", "}"],
-  length: window.prompt("Please pick a password length", "Enter a number between 8 and 128")
+  allCharacters: [],
+  length: window.prompt("Please pick a password length", "Enter a number between 8 and 128"),
+  text: ""
 }
 
-if (/*password.length <= 7 || password.length >= 129 && */password.length === "number") {
+/*if (password.length !== NaN) {
+  window.alert ("Must be a NUMBER and be between 8 and 128");
+ } else {
+   console.log(password.length);
+ }
+*/
+
+
+if (password.length <= 7 || password.length >= 129 || isNaN(parseInt(password.length))) {
  window.alert ("Must be a NUMBER and be between 8 and 128");
 } else {
-  console.log(password.length);
+  var incldNum = window.confirm("Do you want numbers in your password?");
+  var incldLowerCase = window.confirm("Do you want lowercase letters in your password?");
+  var incldUpperCase = window.confirm("Do you want uppercase letters in your password?");
+  var incldSymbols = window.confirm("Do you want symbols in your password?");
+
+  if (incldNum) {
+    // password.text += password.numbers[Math.floor(Math.random() * 10)];
+    password.allCharacters = password.allCharacters.concat(password.numbers)
+  }
+  if (incldLowerCase) {
+    // password.text += password.lowerCase[Math.floor(Math.random() * 26)];
+    password.allCharacters = password.allCharacters.concat(password.lowerCase)
+  }
+  if (incldUpperCase) {
+    password.allCharacters = password.allCharacters.concat(password.upperCase)
+  }
+  if (incldSymbols) {
+    password.allCharacters = password.allCharacters.concat(password.symbols)
+  } 
+
+  for (var i=0; i < password.length; i++) {
+    password.text += password.allCharacters[Math.floor(Math.random() * password.allCharacters.length)];
+  }
+  
+  console.log(password.text)
 }
 
+console.log(password.lowerCase[Math.floor(Math.random() * 26)], password.numbers[Math.floor(Math.random() * 10)], password.upperCase[Math.floor(Math.random() * 26)], password.symbols[Math.floor(Math.random() * 26)]);
 //console.log(password.length);
 //sudo code
+for (var i=0; i < password.length; i++) {
+  password.text += password.lowerCase[Math.floor(Math.random() * 10)];
+}
+
+console.log(password.text)
+
+// var i = 0;
+
+// while(i < password.length) {
+//   password.text += password.numbers[Math.floor(Math.random() * 10)];
+//   i++;
+// }
+
+// do {
+//   password.text += password.numbers[Math.floor(Math.random() * 10)];
+// }while(i < password.length)
+
