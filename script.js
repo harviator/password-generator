0ecaa102b89d708925d7eb08ce597d8a13d51a98
Harviator <78506767+harviator@users.noreply.github.com>
2021-05-20 08:34:00 -0400

    deleted all my experimental code that was commited out.  Just need to figure out how to get a number to always be there if the user chooses a password length that is small.  The easiest and ugliest way is to make the numbers array as big as the other arrays, but I want to find a more elegant way to do this

--- script.js
+++ script.js
@@ -53,56 +53,5 @@ function generatePassword() {
     }
     
     return password.text;
-    //console.log(password.text)
   }
 }
-
-/* Commented out all the experimenting I did
-
-//arrays
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "~", "?", "<", ">", ":", ";", "/", " ", ".", ".", "{", "}"];
-
-console.log(numbers, lowercase, uppercase, symbols);
-
-//random number between 1-26
-console.log(Math.floor(Math.random() * 26));
-
-//now I want to take one of my arrays and randomize it
-console.log(lowercase[Math.floor(Math.random() * 26)]);
-
-//all my arrays randomized
-console.log(lowercase[Math.floor(Math.random() * 26)], numbers[Math.floor(Math.random() * 10)], uppercase[Math.floor(Math.random() * 26)], symbols[Math.floor(Math.random() * 26)]);
-
-//concat all my arrays
-var allCharacters = numbers.concat(lowercase, uppercase, symbols);
-console.log(allCharacters);
-
-//now I can randomize my new array that includes all my arrays
-console.log(allCharacters[Math.floor(Math.random() * 88)]);
-
-//working on increasing string length
-console.log(allCharacters[Math.floor(Math.random() * 88)].repeat(128));
-
-console.log(password.lowerCase[Math.floor(Math.random() * 26)], password.numbers[Math.floor(Math.random() * 10)], password.upperCase[Math.floor(Math.random() * 26)], password.symbols[Math.floor(Math.random() * 26)]);
-//console.log(password.length);
-//sudo code
-for (var i=0; i < password.length; i++) {
-  password.text += password.lowerCase[Math.floor(Math.random() * 10)];
-}
-
-console.log(password.text)
-
-// var i = 0;
-
-// while(i < password.length) {
-//   password.text += password.numbers[Math.floor(Math.random() * 10)];
-//   i++;
-// }
-
-// do {
-//   password.text += password.numbers[Math.floor(Math.random() * 10)];
-// }while(i < password.length)
-*/
